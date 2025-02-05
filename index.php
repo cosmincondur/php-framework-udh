76e4f514f015892e3548898dbeadfc685ae42dda
Cosmin Condur <cosmincondur@outlook.com>
2025-02-05 20:35:51 +0200

    creating files

--- index.php
+++ index.php
@@ -1,27 +1,7 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=pfu_dave_hall;charset=utf8;port:3306";
+require "Model.php";
+$model = new Model;
+$products = $model->getData();
 
-$pdo = new PDO($dsn, "root", "");
-
-$stmt = $pdo->query("SELECT * FROM product");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Product</h1>
-    <?php foreach ($products as $product): ?>
-        <h2><?= $product["name"] ?></h2>
-        <p><?= $product["description"] ?></p>
-    <?php endforeach; ?>
-</body>
-</html>
+require "view.php";
