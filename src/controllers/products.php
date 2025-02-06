5400689a3b641b689c7b3feee6bfcc1b8c495a36
Cosmin Condur <cosmincondur@outlook.com>
2025-02-06 20:21:44 +0200

    working..

--- src/controllers/products.php
+++ src/controllers/products.php
@@ -1,11 +1,18 @@
 <?php
 
-class Products {
-    public function index() {
+class Products
+{
+    public function index()
+    {
         require "src/models/product.php";
-$model = new Product;
-$products = $model->getData();
+        $model = new Product;
+        $products = $model->getData();
 
-require "views/products_index.php";
+        require "views/products_index.php";
+    }
+
+    public function show()
+    {
+        require "views/products_show.php";
     }
 }
--- src/models/product.php
+++ src/models/product.php
@@ -1,15 +1,16 @@
 <?php
 
-class Product {
-    public function getData(): array {
+class Product
+{
+    public function getData(): array
+    {
 
-$dsn = "mysql:host=localhost;dbname=pfu_dave_hall;charset=utf8;port:3306";
+        $dsn = "mysql:host=localhost;dbname=pfu_dave_hall;charset=utf8;port:3306";
 
-$pdo = new PDO($dsn, "root", "");
+        $pdo = new PDO($dsn, "root", "");
 
-$stmt = $pdo->query("SELECT * FROM product");
+        $stmt = $pdo->query("SELECT * FROM product");
 
-return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
