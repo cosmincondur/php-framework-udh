f9dffb299be72f209d2bcb1e2344a72722dc08f0
Cosmin Condur <cosmincondur@outlook.com>
2025-02-13 21:39:07 +0200

    working on project

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-$action = $_GET["action"];
-$controller = $_GET["controller"];
+#LECTIA 23 -> verifica erorile din link
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$segments = explode("/", $path);
+
+$action = $segments[2];
+$controller = $segments[1];
 
 require "src/controllers/$controller.php";
 
