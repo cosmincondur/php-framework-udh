8e82a222746675ef76f251b905e35a1aa368088c
Cosmin Condur <cosmincondur@outlook.com>
2025-02-16 09:17:57 +0200

    tableplus connected to docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM php:8.1-apache
 # Enable mod_rewrite
 RUN a2enmod rewrite
 
+# Install PDO MySQL extension
+RUN docker-php-ext-install pdo_mysql
+
 # Copy the current directory contents into the container at /var/www/html
 COPY . /var/www/html
 
--- src/models/product.php
+++ src/models/product.php
@@ -4,9 +4,9 @@ class Product
 {
     public function getData(): array
     {
-        $dsn = "mysql:host=localhost;dbname=pfu_dave_hall;charset=utf8;port:3306";
+        $dsn = "mysql:host=db;dbname=phpmvc_udh;charset=utf8;port:3306";
 
-        $pdo = new PDO($dsn, "root", "");
+        $pdo = new PDO($dsn, "root", "root");
 
         $stmt = $pdo->query("SELECT * FROM product");
 
